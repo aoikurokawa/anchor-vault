d0ea8dfeb922a944a60c3744769e973882f552ae
Aoi <ht.so.ik0102@gmail.com>
2022-08-29 17:30:18 +0700

    Update test case

--- programs/crunchy-vs-smooth/src/lib.rs
+++ programs/crunchy-vs-smooth/src/lib.rs
@@ -1,15 +1,48 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("7dJxpeqJYp8rQt3mmV8Y8xp5pKcnbPqzht4T4its77hG");
 
 #[program]
 pub mod crunchy_vs_smooth {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let vote_account = &mut ctx.accounts.vote_account;
+        vote_account.crunchy = 0;
+        vote_account.smooth = 0;
+        Ok(())
+    }
+
+    pub fn vote_crunchy(ctx: Context<Vote>) -> Result<()> {
+        let vote_account = &mut ctx.accounts.vote_account;
+        vote_account.crunchy += 1;
+        Ok(())
+    }
+
+    pub fn vote_smooth(ctx: Context<Vote>) -> Result<()> {
+        let vote_account = &mut ctx.accounts.vote_account;
+        vote_account.smooth += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
+    pub vote_account: Account<'info, VoteAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Vote<'info> {
+    #[account(mut)]
+    pub vote_account: Account<'info, VoteAccount>,
+}
+
+#[account]
+pub struct VoteAccount {
+    pub crunchy: u64,
+    pub smooth: u64,
+}
--- tests/crunchy-vs-smooth.ts
+++ tests/crunchy-vs-smooth.ts
@@ -1,16 +1,78 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { CrunchyVsSmooth } from "../target/types/crunchy_vs_smooth";
+import assert from "assert";
 
 describe("crunchy-vs-smooth", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();  
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.CrunchyVsSmooth as Program<CrunchyVsSmooth>;
+  
+  const voteAccount = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
+  it("Initializes with 0 votes for crunchy and smooth", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    console.log("Testing Initialize...");
+    
+    await program.methods
+      .initialize()
+      .accounts({
+        voteAccount: voteAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([voteAccount])
+      .rpc();
+    
+    const account = await program.account.voteAccount.fetch(
+      voteAccount.publicKey
+    );
+    console.log("Crunchy: ", account.crunchy.toString());
+    console.log("Smooth: ", account.smooth.toString());
+    assert.ok(
+      account.crunchy.toNumber() == 0 && account.smooth.toNumber() == 0 
+    );
   });
+  
+  it("Votes correctly for crunchy", async () => {
+    console.log("Testing voteCrunchy...");
+    await program.methods
+      .voteCrunchy()
+      .accounts({
+        voteAccount: voteAccount.publicKey,
+      })
+      .rpc();
+    
+    const account = await program.account.voteAccount.fetch(
+      voteAccount.publicKey
+    );
+
+    console.log("Crunchy: ", account.crunchy.toString());
+    console.log("Smooth: ", account.smooth.toString());
+    assert.ok(
+      account.crunchy.toNumber() == 1 && account.smooth.toNumber() == 0 
+    );
+  })
+
+  it("Votes correctly for smooth", async () => {
+    console.log("Testing voteSmooth...");
+    await program.methods
+      .voteSmooth()
+      .accounts({
+        voteAccount: voteAccount.publicKey,
+      })
+      .rpc();
+    
+    const account = await program.account.voteAccount.fetch(
+      voteAccount.publicKey
+    );
+
+    console.log("Crunchy: ", account.crunchy.toString());
+    console.log("Smooth: ", account.smooth.toString());
+    assert.ok(
+      account.crunchy.toNumber() == 1 && account.smooth.toNumber() == 1 
+    );
+  })
 });
