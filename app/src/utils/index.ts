6275d9bd7087c5be4af784298a8ecfdcc4a3b5ab
Aoi <ht.so.ik0102@gmail.com>
2022-08-30 07:51:24 +0700

    Fix the error in app

--- app/src/utils/index.ts
+++ app/src/utils/index.ts
@@ -9,7 +9,7 @@ export const capitalize = (string: any) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-export const percentize = (decimal: any, min: any, max: any) => {
+export const percentize = (decimal: any, min?: any, max?: any) => {
   if (typeof decimal !== "number" || isNaN(decimal)) return "";
   return max === 0
     ? `${(decimal * 100).toFixed(0)}%`
