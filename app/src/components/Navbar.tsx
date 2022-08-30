3def9b161bc1719929c5c69341ba8f4ad9b0d3c5
Aoi <ht.so.ik0102@gmail.com>
2022-08-30 10:22:33 +0700

    Add Icon

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { AppBar, Container, Toolbar } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { WalletMultiButton } from "@solana/wallet-adapter-material-ui";
+import Image from "next/image";
 import React from "react";
 
 const useStyles = makeStyles((theme: any) => ({
@@ -20,7 +21,7 @@ export default function Navbar() {
     <AppBar position="static" className={classes.root}>
       <Container maxWidth="xl">
         <Toolbar className={classes.toolbar}>
-          <img src="/images/pb-icon.svg" alt="PB Vote" height={40} />
+          <Image src="/pb-icon.svg" alt="PB Vote" height={40} width={40} />
           <WalletMultiButton />
         </Toolbar>
       </Container>
